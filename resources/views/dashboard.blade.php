508d4c4955c083a19590482f5fca431c97c8d0cd
Aymeric <aymeric@macbook-pro-de-aymeric.home>
2021-02-09 12:06:45 +0100

    push git

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,15 @@
                 </ul>
             </div>
         </nav>
-        
+        <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-jet-dropdown-link href="{{ route('logout') }}"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    {{ __('Logout') }}
+                                </x-jet-dropdown-link>
+        </form>
     </div>
 </x-guest-layout>
 
