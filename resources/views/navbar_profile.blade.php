12b449ae34e8eb14d3bfa2391b3b54eb528e375f
Aymeric <aymeric@macbook-pro-de-aymeric.home>
2021-02-09 15:24:58 +0100

    new commit

--- resources/views/navbar_profile.blade.php
+++ resources/views/navbar_profile.blade.php
@@ -1,11 +1,15 @@
 <nav class="navbar fixed-bottom navbar-expand-sm" style="height: 65px; background-color: grey; border-top-left-radius:25px; border-top-right-radius:25px;">
 
+    <a href="{{ route('dashboard') }}">
+        <img width="30px;" src="/images/home.png">
+    </a>
+
     <a href="{{ route('profile') }}">
         <img width="30px;" src="/images/user.png">
     </a>
 
     <a href="{{ route('profile') }}">
-        <img width="30px;" src="/images/user.png">
+        <img width="30px;" src="/images/map.png">
     </a>
 
     <a href="{{ route('profile') }}">
