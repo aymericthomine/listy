826cb894acf7b4b501d2e832093dd94de8f884ba
Aymeric <aymeric@macbook-pro-de-aymeric.home>
2021-02-11 14:47:38 +0100

    new commit

--- config/mail.php
+++ config/mail.php
@@ -98,5 +98,36 @@ return [
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
+    ],
+    'mailjet' => [
+        'key' => env('MAILJET_APIKEY'),
+        'secret' => env('MAILJET_APISECRET'),
+        'transactional' => [
+            'call' => true,
+            'options' => [
+                'url' => 'api.mailjet.com',
+                'version' => 'v3.1',
+                'call' => true,
+                'secured' => true
+            ]
+        ],
+        'common' => [
+            'call' => true,
+            'options' => [
+                'url' => 'api.mailjet.com',
+                'version' => 'v3',
+                'call' => true,
+                'secured' => true
+            ]
+        ],
+        'v4' => [
+            'call' => true,
+            'options' => [
+                'url' => 'api.mailjet.com',
+                'version' => 'v4',
+                'call' => true,
+                'secured' => true
+            ]
+        ],
     ]
 ];
